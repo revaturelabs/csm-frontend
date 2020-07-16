7551693560e44f7ba612855cfc25ae48e2fc67de
Felix Echavarria <echavarria.f@gmail.com>
2020-07-16 15:26:45 -0400

    Solve Mergin Conflict

--- src/App.js
+++ src/App.js
@@ -1,29 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-<<<<<<< HEAD
-import DisplayAssociates from './components/DisplayAssociates/DisplayAssociates'
-
-function App() {
-  return (
-    <DisplayAssociates />
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
-=======
 import Routing from './components/Routing/Routing';
 
 function App() {
@@ -31,7 +8,6 @@ function App() {
     <div>
       <Routing></Routing>
     </div>
->>>>>>> 2fe32a64e099a34d48265e72963a87cfd7c255c5
   );
 }
 
--- src/components/DisplayAssociates/DisplayAssociates.js
+++ src/components/DisplayAssociates/DisplayAssociates.js
@@ -3,18 +3,11 @@ import { useSelector, useDispatch } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 import { Button, Table, Icon, Popup, Modal, Input } from 'semantic-ui-react';
 import './DisplayAssociates.css';
-<<<<<<< HEAD
-import AssociateService from '../../services/associate.service.js';
-
-const DisplayAssociates = (props) => {
-    const state = useSelector(state => state);
-=======
 import DisplayAssociate from './DisplayAssociate';
 import AssociateService from '../../services/associate.service.js';
 
 const DisplayAssociates = (props) => {
     const associatesState = useSelector(state => state.associateReducer);
->>>>>>> 2fe32a64e099a34d48265e72963a87cfd7c255c5
     const dispatch = useDispatch();
     const history = useHistory();
     const associateService = new AssociateService();
@@ -22,28 +15,10 @@ const DisplayAssociates = (props) => {
 
     useEffect(() => {
         console.log("useEffect")
-<<<<<<< HEAD
 
         getAssociatesInformation();
     }, []);
 
-    async function getAssociatesInformation() {
-        let resp = await associateService.getAssociatesInformation()
-        console.log(resp)
-        dispatch({ type: 'setAssociatesInformation', requests: resp.data })
-    }
-
-    const handleClickOpen = () => {
-        setOpen(true);
-    };
-=======
->>>>>>> 2fe32a64e099a34d48265e72963a87cfd7c255c5
-
-        getAssociatesInformation();
-    }, []);
-
-<<<<<<< HEAD
-=======
     // TODO: Get real manager id
     const testManager = {id: '{managerId}'}
 
@@ -54,7 +29,6 @@ const DisplayAssociates = (props) => {
         console.log(associatesState.associates)
     }
 
->>>>>>> 2fe32a64e099a34d48265e72963a87cfd7c255c5
     return (
         <div>
             <header>List of Associates</header>
@@ -69,57 +43,8 @@ const DisplayAssociates = (props) => {
                         </Table.Row>
                     </Table.Header>
                     <Table.Body>
-<<<<<<< HEAD
-                        {/* {Array.isArray(state.associates) ? state.associates.map(
-                            (request) => {
-                                return ( */}
-                        <Table.Row>
-                            {/* <td>{request.email}</td>
-                                        <td>{request.name}</td>
-                                        <td>{request.batch}</td> */}
-                            <Table.Cell>{data.email}</Table.Cell>
-                            <Table.Cell>{data.name}</Table.Cell>
-                            <Table.Cell>{data.batch}</Table.Cell>
-                            <Popup content='View Information' trigger={
-                                <div class="circular ui icon button" onClick={handleClickOpen}>
-                                    <i class="icon eye"></i></div>
-                            } />
-                        </Table.Row>
-                        <Modal
-                            open={open}
-                            onClose={handleClose}>
-                            <Modal.Header> Employee Information</Modal.Header>
-                            <Modal.Content>
-                                <Modal.Description>
-                                    <div class="ui input">
-                                        <label>Email:</label>
-                                        <input class="ui input" type="email" value={data.email} />
-                                    </div>
-                                    <br />
-                                    <div class="ui input">
-                                        <label>Name:</label>
-                                        <input class="ui input" type="text" value={data.name} />
-                                    </div>
-                                    <br />
-                                    <div class="ui input">
-                                        <label>Batch:</label>
-                                        <input type="text" value={data.batch} />
-                                    </div>
-                                </Modal.Description>
-                            </Modal.Content>
-                            <Modal.Actions>
-                                <Button primary>
-                                    Proceed <Icon name='right chevron' />
-                                </Button>
-                            </Modal.Actions>
-                        </Modal>
-                        {/*  )
-                             }
-                         ) : null} */}
-=======
                         {Array.isArray(associatesState.associates) ? associatesState.associates.map((associate) => { return ( <DisplayAssociate associate={associate}/> )
                         }): null }
->>>>>>> 2fe32a64e099a34d48265e72963a87cfd7c255c5
                     </Table.Body>
                 </Table>
             </div>

f20ae97b52e608e04e75c4bcdc82e9475481f2d4
jcarevature <Justin.Adams@uvm.edu>
2020-07-16 18:36:01 -0400

    Added todo to change categories service to the back end uri/categories.

--- src/services/categories.service.js
+++ src/services/categories.service.js
@@ -2,6 +2,7 @@ const { default: axios } = require('axios')
 
 class CategoryService {
     constructor() {
+        // TODO: Change this URI to the backend_uri/categories from the back end team.
         this.URI = 'http://34.82.182.44:80/mock/category/category';
     }
 

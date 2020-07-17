17305ece69f98c78837eb3f0510ba1d06b9f6191
jcarevature <Justin.Adams@uvm.edu>
2020-07-17 17:31:34 -0400

    Pushing testing configuration.

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Route, BrowserRouter as Router } from "react-router-dom";
 import { render } from '@testing-library/react';
 import { useDispatch } from 'react-redux';
-import App from './App';
 
 function App() {
   return (
--- src/components/Search/Search.test.js
+++ src/components/Search/Search.test.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import Search from './Search.js';
-describe('MyComponent', () => {
-  it('should render correctly in "debug" mode', () => {
+import { useDispatch } from "react-redux";
+
+describe('Test Search.js', () => {
+  it('Should render Search.js correctly in "debug" mode.', () => {
     const component = shallow(<Search debug />);
     expect(component).toMatchSnapshot();
   });

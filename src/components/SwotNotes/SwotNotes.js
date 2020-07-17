550d177f39688516e6a4deaa9461b482eb7ee558
troyalcaraz <67602016+troyalcaraz@users.noreply.github.com>
2020-07-17 15:43:46 -0500

    more comments

--- src/components/SwotNotes/SwotNotes.js
+++ src/components/SwotNotes/SwotNotes.js
@@ -5,7 +5,8 @@ import { useSelector, useDispatch } from "react-redux";
 /**
  * SwotNotes is a functional component that stores a string that
  * represents general notes for an individual associate and is a 
- * sibling to the swotQuadrant component.
+ * sibling to the swotQuadrant component. When text is typed
+ * into the text area it updates the notes in the store automatically.
  * @param {*} props Properties from the SwotModal component
  * @returns a text area with notes on an associate
  */

6623c9c65fa3e49be0accd7e267747fc8a09ac6c
jcarevature <Justin.Adams@uvm.edu>
2020-07-17 18:06:27 -0400

    Finishing test cases for the Jest/Enzyme functionality.

--- src/components/Search/Search.test.js
+++ src/components/Search/Search.test.js
@@ -2,39 +2,46 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import Search from './Search.js';
 import { useDispatch } from "react-redux";
+import init from "../../testStore/swotStoreTest.js";
 
 describe('Test Search.js', () => {
+    let initialState = init;
     it('Should render Search.js correctly without props.', () => {
-      const component = shallow(<Search/>);
-
-      expect(component).toMatchSnapshot();
+        const component = shallow(<Search/>);
+        expect(component).toMatchSnapshot();
     });
     it('Should render with props. Define props for your object below.', () => {
-      const strings = ['one', 'two'];
-      const component = shallow(<Search list={strings} />);
-      expect(component).toMatchSnapshot();
+        const id = "search";
+        const icon = "search";
+        const placeholder = "Search...";
+        const component = shallow(<Search id={id} icon={icon} placeholder={placeholder} />);
+        expect(component).toMatchSnapshot();
     });
     it('Should be possible to activate button with Spacebar. Define user input below.', () => {
         const component = mount(<Search />);
         component
-          .find('button#my-button-one')
-          .simulate('keydown', { keyCode: 32 });
+            .find('#search');
+            .simulate('keydown', { keyCode: 65 });
+            .simulate('keydown', { keyCode: 87 });
+            .simulate('keydown', { keyCode: 83 });
         expect(component).toMatchSnapshot();
         component.unmount();
     });
-    it('Button click should hide component', () => {
-        const component = shallow(<Search onClick={clickFn} />);
+    it('Search component click should find an object.', () => {
+        const component = shallow(<Search onChange={find} />);
         component
-          .find('button#my-button-two')
-          .simulate('click');
-        expect(clickFn).toHaveBeenCalled();
+            .find('#search');
+            .simulate('keydown', { keyCode: 65 });
+            .simulate('keydown', { keyCode: 87 });
+            .simulate('keydown', { keyCode: 83 });
+        expect(find).toHaveBeenCalled();
     });
     it('should set storage on save button click', () => {
         mockTryGetValue.mockReturnValueOnce(true);
-        const component = mount(<MyComponent />);
-        component.find('button#my-button-three').simulate('click');
+        const component = mount(<Search />);
+        component.find('#search').simulate('keydown', {keyCode: 65 });
         expect(mockTryGetValue).toHaveBeenCalled();
         expect(component).toMatchSnapshot();
         component.unmount();
-    }); 
+    });
 });

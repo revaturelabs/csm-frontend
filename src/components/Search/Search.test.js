f7f0a6d339c2747864e35967ed96727d02fb7ea1
jcarevature <Justin.Adams@uvm.edu>
2020-07-17 17:42:25 -0400

    Adding React function testing example.

--- src/components/Search/Search.test.js
+++ src/components/Search/Search.test.js
@@ -5,21 +5,28 @@ import { useDispatch } from "react-redux";
 
 describe('Test Search.js', () => {
     it('Should render Search.js correctly without props.', () => {
-      const component = shallow(<MyComponent/>);
+      const component = shallow(<Search/>);
 
       expect(component).toMatchSnapshot();
     });
     it('Should render with props. Define props for your object below.', () => {
       const strings = ['one', 'two'];
-      const component = shallow(<MyComponent list={strings} />);
+      const component = shallow(<Search list={strings} />);
       expect(component).toMatchSnapshot();
     });
     it('Should be possible to activate button with Spacebar. Define user input below.', () => {
-        const component = mount(<MyComponent />);
+        const component = mount(<Search />);
         component
           .find('button#my-button-one')
           .simulate('keydown', { keyCode: 32 });
         expect(component).toMatchSnapshot();
         component.unmount();
+    });
+    it('Button click should hide component', () => {
+        const component = shallow(<Search onClick={clickFn} />);
+        component
+          .find('button#my-button-two')
+          .simulate('click');
+        expect(clickFn).toHaveBeenCalled();
     });
 });

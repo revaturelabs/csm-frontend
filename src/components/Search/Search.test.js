aadb2b13fe9480e124d57eaa4fabfc053e2105ad
jcarevature <Justin.Adams@uvm.edu>
2020-07-18 16:12:02 -0400

    Completed a testing library example for React, Jest, Enzyme, and Hooks.

--- src/components/Search/Search.test.js
+++ src/components/Search/Search.test.js
@@ -18,54 +18,54 @@ jest.mock("react-redux", () => ({
 
 describe("Search unit test", () => {
 
-	it('Rendering component without props.', () => {
-			const component = shallow(<Search/>);
-			expect(component).toMatchSnapshot();
-	});
+  	it('Rendering component without props.', () => {
+  			const component = shallow(<Search/>);
+  			expect(component).toMatchSnapshot();
+  	});
 
-	it('Rendering component with props that match the original component.', () => {
-			const id="search";
-			const find = jest.fn();
-			const icon="search";
-			const placeholder='Search...';
-		  const component = shallow(<Search id={id} icon={icon} fluid="true" onChange={find} placeholder={placeholder} />);
-		  expect(component).toMatchSnapshot();
-	});
+  	it('Rendering component with props that match the original component.', () => {
+  			const id="search";
+  			const find = jest.fn();
+  			const icon="search";
+  			const placeholder='Search...';
+  		  const component = shallow(<Search id={id} icon={icon} fluid="true" onChange={find} placeholder={placeholder} />);
+  		  expect(component).toMatchSnapshot();
+  	});
 
-	it('Mounting, testing user input, and dismounting the component.', () => {
-			const id="search";
-			const find = jest.fn();
-			const icon="search";
-			const placeholder='Search...';
-			const component = mount(<Search id={id} icon={icon} fluid="true" onChange={find} placeholder={placeholder} />);
-		  component.find('#search').value;
-			component.simulate('keypress', { keyCode: 'A' });
-			component.simulate('keypress', { keyCode: 'W' });
-			component.simulate('keypress', { keyCode: 'S' });
-		  expect(component).toMatchSnapshot();
-		  component.unmount();
-	});
+  	it('Mounting, testing user input, and dismounting the component.', () => {
+  			const id="search";
+  			const find = jest.fn();
+  			const icon="search";
+  			const placeholder='Search...';
+  			const component = mount(<Search id={id} icon={icon} fluid="true" onChange={find} placeholder={placeholder} />);
+  		  component.find('#search').value;
+  			component.simulate('keypress', { keyCode: 'A' });
+  			component.simulate('keypress', { keyCode: 'W' });
+  			component.simulate('keypress', { keyCode: 'S' });
+  		  expect(component).toMatchSnapshot();
+  		  component.unmount();
+  	});
 
   	it('Testing find function call.', () => {
-		const { useSelector, useDispatch } = require("react-redux");
-	    useSelector.mockImplementation((callback) => {
-	      return callback({
-	        swotReducer: {
-	          categories: ["AWS", "Python", "Javascript"],
-	          displayCategories: [],
-	        },
-	      });
-		});
-		useDispatch.mockImplementation(() => {
-			return reducer
-		});
-		const id="search";
-		const find = jest.fn();
-		const icon="search";
-		const placeholder='Search...';
-		const event = {target: {name: "search", value: "AWS"}};
-		const component = mount(<Search id={id} icon={icon} fluid="true" onChange={find} placeholder={placeholder} />);
-		component.find('input').simulate('change', event);
-	    expect(find).toHaveBeenCalled();
+    		const { useSelector, useDispatch } = require("react-redux");
+    	    useSelector.mockImplementation((callback) => {
+    	      return callback({
+    	        swotReducer: {
+    	          categories: ["AWS", "Python", "Javascript"],
+    	          displayCategories: [],
+    	        },
+    	      });
+    		});
+    		useDispatch.mockImplementation(() => {
+    			   return store;
+    		});
+    		const id="search";
+    		const find = jest.fn();
+    		const icon="search";
+    		const placeholder='Search...';
+    		const event = {target: {name: "search", value: "AWS"}};
+    		const component = mount(<Search id={id} icon={icon} fluid="true" onChange={find} placeholder={placeholder} />);
+    		component.find('input').simulate('change', event);
+    	  component.expect(find).toHaveBeenCalled();
   	});
 });

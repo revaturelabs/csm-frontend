a1c69dfc0d2f3e15316d242a6af4b6bde5436a4f
jcarevature <Justin.Adams@uvm.edu>
2020-07-18 16:46:49 -0400

    Updating test suite.

--- src/components/Search/Search.test.js
+++ src/components/Search/Search.test.js
@@ -16,52 +16,52 @@ jest.mock('react-redux');
 describe("Search unit test", () => {
 
   	it('Rendering component without props.', () => {
-  			const component = shallow(<Search/>);
-  			expect(component).toMatchSnapshot();
+        const component = shallow(<Search/>);
+        expect(component).toMatchSnapshot();
   	});
 
   	it('Rendering component with props that match the original component.', () => {
-  			const id="search";
-  			const find = jest.fn();
-  			const icon="search";
-  			const placeholder='Search...';
-  		  const component = shallow(<Search id={id} icon={icon} fluid="true" onChange={find} placeholder={placeholder} />);
-  		  expect(component).toMatchSnapshot();
+        const id="search";
+        const find = jest.fn();
+        const icon="search";
+        const placeholder='Search...';
+        const component = shallow(<Search id={id} icon={icon} fluid="true" onChange={find} placeholder={placeholder} />);
+        expect(component).toMatchSnapshot();
   	});
 
   	it('Mounting, testing user input, and dismounting the component.', () => {
-  			const id="search";
-  			const find = jest.fn();
-  			const icon="search";
-  			const placeholder='Search...';
-  			const component = mount(<Search id={id} icon={icon} fluid="true" onChange={find} placeholder={placeholder} />);
-  		  component.find('#search').value;
-  			component.simulate('keypress', { keyCode: 'A' });
-  			component.simulate('keypress', { keyCode: 'W' });
-  			component.simulate('keypress', { keyCode: 'S' });
-  		  expect(component).toMatchSnapshot();
-  		  component.unmount();
+        const id="search";
+        const find = jest.fn();
+        const icon="search";
+        const placeholder='Search...';
+        const component = mount(<Search id={id} icon={icon} fluid="true" onChange={find} placeholder={placeholder} />);
+        component.find('#search').value;
+        component.simulate('keypress', { keyCode: 'A' });
+        component.simulate('keypress', { keyCode: 'W' });
+        component.simulate('keypress', { keyCode: 'S' });
+        expect(component).toMatchSnapshot();
+        component.unmount();
   	});
 
   	it('Testing find function call.', () => {
         const dispatch = jest.fn();
-    		const { useSelector, useDispatch } = require("react-redux");
-    	    useSelector.mockImplementation((callback) => {
-    	      return callback({
+		const { useSelector, useDispatch } = require("react-redux");
+        useSelector.mockImplementation((callback) => {
+            return callback({
     	        swotReducer: {
-    	          categories: ["AWS", "Python", "JavaScript"],
-    	          displayCategories: [],
+                    categories: ["AWS", "Python", "JavaScript"],
+                    displayCategories: []
     	        },
-    	      });
-    		});
-    		useDispatch.mockReturnValue(dispatch);
-    		const id="search";
-    		const find = jest.fn();
-    		const icon="search";
-    		const placeholder='Search...';
-    		const event = {target: {name: "search", value: "A"}};
-    		const component = mount(<Search id={id} icon={icon} fluid="true" onChange={find} placeholder={placeholder} />);
-    		component.find('input').simulate('change', event);
-    	  expect(dispatch).toBeCalledWith({type: "updateDisplayCategories", getDisplayCategories: ['AWS', 'JavaScript']})
+            });
+		});
+        useDispatch.mockReturnValue(dispatch);
+        const id="search";
+        const find = jest.fn();
+        const icon="search";
+        const placeholder='Search...';
+        const event = {target: {name: "search", value: "A"}};
+        const component = mount(<Search id={id} icon={icon} fluid="true" onChange={find} placeholder={placeholder} />);
+        component.find('input').simulate('change', event);
+        expect(dispatch).toBeCalledWith({type: "updateDisplayCategories", getDisplayCategories: ['AWS', 'JavaScript']})
   	});
 });

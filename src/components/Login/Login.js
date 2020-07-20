41812ddb092f748aaf7bb8e66dbdda0654908854
Felix Echavarria <echavarria.f@gmail.com>
2020-07-20 18:28:32 -0400

    Moving the style to its own file

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -20,20 +20,20 @@ const Login = (props) => {
     }
 
     return (
-        <Grid id='grid' textAlign='center' style={{ height: '100vh' }} verticalAlign='middle'>
-            <Grid.Column style={{ maxWidth: 450 }}>
+        <Grid id='grid'>
+            <Grid.Column id='gridcolumn'>
             <Image src='https://app.revature.com/core/resources/download/organizations/logos/89c5d424854a06ca216c885f43550bcc.png/empImage' 
             verticalAlign='middle'/>
-                <Header as='h2' color='orange' textAlign='center'>
+                <Header id='title'>
                     Log-in to your account
-      </Header>
-                <Form size='large'>
+                </Header>
+                <Form id='form'>
                     <Segment stacked>
                         <Form.Input fluid icon='user' iconPosition='left' placeholder='E-mail address' />
 
-                        <Button color='orange' fluid size='large' onClick={login} >
+                        <Button id='btn' fluid onClick={login} >
                             Login
-          </Button>
+                        </Button>
                     </Segment>
                 </Form>
             </Grid.Column>

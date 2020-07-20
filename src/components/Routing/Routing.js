4d1da6d12ee33ebc203fe7de1df87711b515bccd
Jesse Stoler <jesse.stoler@gmail.com>
2020-07-20 17:58:07 -0600

    fixed qc routing

--- src/components/Routing/Routing.js
+++ src/components/Routing/Routing.js
@@ -3,12 +3,14 @@ import { Route, BrowserRouter as Router } from "react-router-dom";
 
 import DisplayAssociates from '../DisplayAssociates/DisplayAssociates';
 import SpiderChart from '../SpiderCharts/SpiderChart';
+import QC from '../QC/qc'
 
 const Routing = (props) => {
   return (
     <Router>
       <Route exact path='/promotedlastweek' component={DisplayAssociates} />
       <Route exact path='/spidercharts' component={SpiderChart} />
+      <Route exact path='/qcdata' component={QC} />
     </Router>
   )
 }

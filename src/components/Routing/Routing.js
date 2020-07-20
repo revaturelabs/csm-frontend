d92f3b2e2211d94349e53bc6dac8b3e287a41beb
Felix Echavarria <echavarria.f@gmail.com>
2020-07-20 13:47:00 -0400

    Adding login

--- src/components/Routing/Routing.js
+++ src/components/Routing/Routing.js
@@ -2,14 +2,12 @@ import React from "react";
 import { Route, BrowserRouter as Router } from "react-router-dom";
 
 import DisplayAssociates from '../DisplayAssociates/DisplayAssociates';
-import SpiderChart from '../SpiderCharts/SpiderChart';
 import Login from '../Login/Login'
 
 const Routing = (props) => {
   return (
     <Router>
       <Route exact path='/promotedlastweek' component={DisplayAssociates} />
-      <Route exact path='/spidercharts' component={SpiderChart} />
       <Route exact path='/login' component={Login} />
     </Router>
   )

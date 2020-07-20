2a833b7debd7197634b22f52453252ab28295505
Felix Echavarria <echavarria.f@gmail.com>
2020-07-20 13:52:11 -0400

    Removing Login from this branch

--- src/components/Routing/Routing.js
+++ src/components/Routing/Routing.js
@@ -3,14 +3,12 @@ import { Route, BrowserRouter as Router } from "react-router-dom";
 
 import DisplayAssociates from '../DisplayAssociates/DisplayAssociates';
 import SpiderChart from '../SpiderCharts/SpiderChart';
-import Login from '../Login/Login'
 
 const Routing = (props) => {
   return (
     <Router>
       <Route exact path='/promotedlastweek' component={DisplayAssociates} />
       <Route exact path='/spidercharts' component={SpiderChart} />
-      <Route exact path='/login' component={Login} />
     </Router>
   )
 }

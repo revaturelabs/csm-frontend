8b74db10ad726fb1f3fbd70c1b98a4bf27a4c5cd
Kevin Haggerty <khaggerty1@crimson.ua.edu>
2020-07-22 13:31:54 -0400

    getting this out of the way

--- src/reducers/displayAssociatesReducer.js
+++ src/reducers/displayAssociatesReducer.js
@@ -2,7 +2,7 @@ const initialState = {
     batches:[], //batches will each contain an array of associates
 }
 
-function displayAssociatesReducer(state = initialState, action) {
+function batchReducer(state = initialState, action) {
     console.log(state);
     console.log(action);
     switch(action.type) {
@@ -15,3 +15,25 @@ function displayAssociatesReducer(state = initialState, action) {
 
 
 export default displayAssociatesReducer;
+
+
+
+
+
+// [batches
+//     {batch1
+//         batchID,
+//         manager
+//         trainer name,
+//         promotion date,
+//         associates [
+//             {associate1
+//                 name,
+//                 # of swots
+//                 email,
+//                 userID (if different than email. Whatever we use to look up evaluations)
+//             }
+//         ]
+//     }
+//     ...
+// ]

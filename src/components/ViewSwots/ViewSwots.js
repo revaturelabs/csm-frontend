c470d7f11859e94e6d64fa5fe7a83f95a6f8f44e
jcarevature <Justin.Adams@uvm.edu>
2020-07-24 15:08:35 -0400

    Updating ViewSwots.js

--- src/components/ViewSwots/ViewSwots.js
+++ src/components/ViewSwots/ViewSwots.js
@@ -5,16 +5,6 @@ import SwotsGrid from "../SwotsGrid/SwotsGrid";
 import SwotsBar from "../SwotsBar/SwotsBar";
 
 const ViewSwots = (props) => {
-    const email = useSelector((state) => state.swotReducer.currentAssociate.email);
-    const dispatch = useDispatch();
-    const swotService = new SwotService();
-    useEffect(() => {
-        async function getSwots() {
-            const swots = await swotService.getSwots(email);
-            dispatch({ type: "updateSwots", getSwots: swots});
-        }
-    getSwots();
-    }, [dispatch]);
   return (
     <>
       <SwotsBar />

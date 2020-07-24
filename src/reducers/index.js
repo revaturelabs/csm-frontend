5e9a3e853741360006a2471dfd161d051fd5e47c
jcarevature <Justin.Adams@uvm.edu>
2020-07-24 14:37:41 -0400

    Resolving merge conflict on index.js.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -7,9 +7,5 @@ import managerReducer from './managerReducer'
 export default combineReducers({
     managerReducer,
     swotReducer,
-<<<<<<< HEAD
     batchReducer
-=======
-    batchReducer,
->>>>>>> e37a0a4a0a1a0b2405471c3bebedd0707067d595
 })

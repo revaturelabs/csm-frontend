699bbef50d6ae2743fef290eecc7194116cdb0d6
Derek <derekjou0@gmail.com>
2020-07-24 15:00:39 -0700

    view swots

--- src/components/Routing/Routing.js
+++ src/components/Routing/Routing.js
@@ -1,17 +1,19 @@
 import React from "react";
 import { Route, BrowserRouter as Router } from "react-router-dom";
 
-import DisplayAssociates from '../DisplayAssociates/DisplayAssociates';
-import Login from '../Login/Login'
+import DisplayAssociates from "../DisplayAssociates/DisplayAssociates";
+import Login from "../Login/Login";
+import ViewSwots from "../ViewSwots/ViewSwots";
 
 const Routing = (props) => {
   return (
     <Router>
-      <Route exact path='/' component={Login} />
-      <Route exact path='/login' component={Login} />
-      <Route exact path='/promotedlastweek' component={DisplayAssociates} />
+      <Route exact path="/" component={Login} />
+      <Route exact path="/login" component={Login} />
+      <Route exact path="/promotedlastweek" component={DisplayAssociates} />
+      <Route path="/viewSwots" component={ViewSwots} />
     </Router>
-  )
-}
+  );
+};
 
 export default Routing;
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,10 @@
 import { combineReducers } from "redux";
-import batchReducer from './batchReducer'
-import managerReducer from './managerReducer'
-
+import batchReducer from "./batchReducer";
+import managerReducer from "./managerReducer";
+import swotReducer from "./swotReducer";
 
 export default combineReducers({
-    batchReducer,
-    managerReducer,
-})
-
+  batchReducer,
+  managerReducer,
+  swotReducer
+});

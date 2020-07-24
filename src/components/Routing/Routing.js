bc510252265fa8ff675ed0f00ce9a9733caeeef5
jcarevature <Justin.Adams@uvm.edu>
2020-07-24 10:29:33 -0400

    Updating changes to routing and package.json

--- src/components/Routing/Routing.js
+++ src/components/Routing/Routing.js
@@ -1,33 +1,21 @@
 import React from "react";
 import { Route, BrowserRouter as Router } from "react-router-dom";
-<<<<<<< HEAD
-=======
 
 import DisplayAssociates from '../DisplayAssociates/DisplayAssociates';
->>>>>>> c835978f343cfb3b4384532d01d68a916cafd0e4
 import Login from '../Login/Login'
 import SwotPage from '../SwotPage/SwotPage'
 import ViewSwots from '../ViewSwots/ViewSwots'
 
 const Routing = (props) => {
-<<<<<<< HEAD
     return (
         <Router>
             <Route exact path='/' component={Login} />
             <Route exact path='/login' component={Login} />
             <Route path="/editSwot" component={SwotPage}/>
             <Route path='/viewSwots' component={ViewSwots}/>
+            <Route exact path='/promotedlastweek' component={DisplayAssociates} />
         </Router>
     )
-=======
-  return (
-    <Router>
-      <Route exact path='/' component={Login} />
-      <Route exact path='/login' component={Login} />
-      <Route exact path='/promotedlastweek' component={DisplayAssociates} />
-    </Router>
-  )
->>>>>>> c835978f343cfb3b4384532d01d68a916cafd0e4
 }
 
 export default Routing;

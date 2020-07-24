0796d0afcdaabe85659f2e1f4b508170d86a7e50
jcarevature <Justin.Adams@uvm.edu>
2020-07-24 12:04:14 -0400

    Resolving merge conflict in search.js

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -6,25 +6,15 @@ const Search = (props) => {
 		const dispatch = useDispatch();
 		const categories = useSelector((state) => state.swotReducer.categories);
 		const findPattern = (event) => {
-				let elements = [];
-				// let re = /^[^`~!@#$%^&*()_+={}\[\]|\\:;“’<,>.?๐฿]*$/;
-				let pattern = event.target.value;
-				pattern = pattern.replace(/[.*+\-?^${}()|[\]\\]/g, '\\$&');
-<<<<<<< HEAD
-				for (category of categories) {
-						if(category.skillCategory.toLowerCase().search(pattern.skillCategory.toLowerCase()) > -1) {
-								elements.push(category);
-						}
-				}
-			dispatch({type: 'updateDisplayCategories', updateDisplayCategories: elements})
-=======
-				for(const category of categories) {
-						if(category.skillCategory.toLowerCase().search(pattern.toLowerCase()) > -1) {
-								elements.push(category);
-						}
-				}
-				dispatch({ type: "updateDisplayCategories", getDisplayCategories: elements })
->>>>>>> 5f04a5a173918847eddf2ea1f4b3ce3999f13547
+			let elements = [];
+			let pattern = event.target.value;
+			pattern = pattern.replace(/[.*+\-?^${}()|[\]\\]/g, '\\$&');
+			for(const category of categories) {
+					if(category.skillCategory.toLowerCase().search(pattern.toLowerCase()) > -1) {
+							elements.push(category);
+					}
+			}
+			dispatch({ type: "updateDisplayCategories", getDisplayCategories: elements })
 		}
 		return (
 			<Input id="search" fluid onChange={findPattern} icon="search" placeholder='Search...' />

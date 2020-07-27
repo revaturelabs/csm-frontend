838ff20e2e5b2769ab177e4cf4ed0bb498eef3c6
troyalcaraz <67602016+troyalcaraz@users.noreply.github.com>
2020-07-27 16:30:46 -0500

    SwotNotes.test.js

--- src/components/SwotNotes/SwotNotes.test.js
+++ src/components/SwotNotes/SwotNotes.test.js
@@ -1,39 +1,31 @@
 // Component.test.js
 import React from "react";
 import ReactDOM from "react-dom";
-import Enzyme, { shallow, mount, render, fireEvent } from 'enzyme';
-import EnzymeAdapter from 'enzyme-adapter-react-16';
-import { useSelector, useDispatch, useShallowEqualSelector } from 'react-redux';
-import configureStore from 'redux-mock-store';
-import reducer from '../../reducers';
-import SwotNotes from './SwotNotes';
+import Enzyme, { shallow, mount, render, fireEvent } from "enzyme";
+import EnzymeAdapter from "enzyme-adapter-react-16";
+import { useSelector, useDispatch, useShallowEqualSelector } from "react-redux";
+import configureStore from "redux-mock-store";
+import swotReducer from "../../reducers/swotReducer";
+import renderer from "react-test-renderer";
+import SwotNotes from "./SwotNotes";
 
 // It can receive two more parameters, the second one is to specify a factory instead of the jest's automocking feature
-jest.mock('react-redux');
+jest.mock("react-redux");
 
 describe("SwotNotes unit test", () => {
+  it("should render component without props", () => {
+    const tree = renderer.create(<SwotNotes />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 
-	it('Rendering component without props.', () => {
-		const component = shallow(<SwotNotes/>);
-		expect(component).toMatchSnapshot();
-	});
+  it("should render component with props", () => {
+    const placeholder='Notes...';
+    const updateNotes = jest.fn();
+    const component = shallow(<SwotNotes onChange={updateNotes} placeholder={placeholder} />);
+    expect(component).toMatchSnapshot();
+  });
 
-	it('Rendering component with props that match the original component.', () => {
-		// Set your own props here...
-		props = "props";
-		const component = render(<SwotNotes props={props} />);
-		expect(component).toMatchSnapshot();
-	});
-
-	it('Mounting, testing user input, and dismounting the component.', () => {
-		// Set your own functions and props here...
-		props = "props";
-		find = jest.fn();
-		const component = mount(<Search id="find" props={props} find={find} />);
-		component.find('#find').value;
-		component.simulate('keypress', { keyCode: 'A' });
-		expect(component).toMatchSnapshot();
-		component.unmount();
-	});
-
+  it('should render the initial state', () => {
+    expect(swotReducer).toEqual(swotReducer)
+  });
 });

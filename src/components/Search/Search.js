2c124cadc164e880a3ee9e50d464880745b07aa1
jcarevature <Justin.Adams@uvm.edu>
2020-07-27 11:37:18 -0400

    Resolving merge conflicts in Search.js

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -3,25 +3,6 @@ import { Input } from "semantic-ui-react";
 import { useSelector, useDispatch } from "react-redux";
 
 const Search = (props) => {
-<<<<<<< HEAD
-		const dispatch = useDispatch();
-		const categories = useSelector((state) => state.swotReducer.categories);
-		const findPattern = (event) => {
-			let elements = [];
-			let pattern = event.target.value;
-			pattern = pattern.replace(/[.*+\-?^${}()|[\]\\]/g, '\\$&');
-			for(const category of categories) {
-					if(category.skillCategory.toLowerCase().search(pattern.toLowerCase()) > -1) {
-							elements.push(category);
-					}
-			}
-			dispatch({ type: "updateDisplayCategories", getDisplayCategories: elements })
-		}
-		return (
-			<Input id="search" fluid onChange={findPattern} icon="search" placeholder='Search...' />
-		)
-}
-=======
   const dispatch = useDispatch();
   const categories = useSelector((state) => state.swotReducer.categories);
   const findPattern = (event) => {
@@ -51,6 +32,5 @@ const Search = (props) => {
     />
   );
 };
->>>>>>> 7ba30ffc46581d1e6d208365e45aca8e74bc14f4
 
 export default Search;

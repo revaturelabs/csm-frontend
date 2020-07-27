d514599370c2779be8b2b537244c55aaf14fc0f0
jcarevature <Justin.Adams@uvm.edu>
2020-07-27 11:53:25 -0400

    Resolving merge conflicts in SwotsBar.js

--- src/components/SwotsBar/SwotsBar.js
+++ src/components/SwotsBar/SwotsBar.js
@@ -1,110 +1,92 @@
-import React from 'react';
-import { useDispatch, useSelector, useCallback } from 'react-redux';
-import DatePicker from 'react-datepicker';
-import { Menu, Button } from 'semantic-ui-react';
-import { useHistory } from 'react-router-dom';
-import SwotService from '../../services/swot.service.js';
-import './SwotsBar.scss';
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import DatePicker from "react-datepicker";
+import { Menu, Button, Icon } from "semantic-ui-react";
+import { useHistory } from "react-router-dom";
+import "./SwotsBar.css";
+import "react-datepicker/dist/react-datepicker.css";
 
 const SwotsBar = (props) => {
+    const startDate = useSelector((state) => state.swotReducer.startDate);
+    const endDate = useSelector((state) => state.swotReducer.endDate);
     const associate = useSelector((state) => state.swotReducer.currentAssociate);
-    const swots = useSelector((state) => state.swotReducer.currentAssociate.swot);
-    let displaySwots = useSelector((state) => state.swotReducer.displaySwots);
-    let startDate = new Date(useSelector((state) => state.swotReducer.currentAssociate.end_date));
-    let endDate = new Date(useSelector((state) => state.swotReducer.endDate));
     const dispatch = useDispatch();
     const history = useHistory();
-    startDate.setDate(startDate.getDate()-14);
-    const filter = (start, end) => {
-        let arr = swots;
-        arr = arr.filter(swot => new Date(swot.date_created) >= new Date(start) && new Date(swot.date_created) <= new Date(end));
-        dispatch({type: 'updateDisplaySwots', getDisplaySwots: arr})
+
+    const roundDate = (date) => {
+        //subtract amount of time since midnight
+        date -= date % (24 * 60 * 60 * 1000);
+        //add on the timezone offset
+        date += new Date().getTimezoneOffset() * 60 * 1000;
+        return new Date(date);
     }
+
+    const filter = (start, end) => {
+    let swots = associate.swot;
+        swots = swots.filter(
+            (swot) =>
+                roundDate(new Date(swot.date_created))
+                >= roundDate(new Date(start)) &&
+                roundDate(new Date(swot.date_created))
+                <= roundDate(new Date(end))
+        );
+        dispatch({ type: "updateDisplaySwots", swots: swots });
+    };
+
     const handleStartDate = (date) => {
         filter(date, endDate);
-        dispatch({type: 'updateStartDate', startDate: date});
-    }
+        dispatch({ type: "updateStartDate", startDate: date });
+    };
 
-  const roundDate = (date) => {
-    date -= date % (24 * 60 * 60 * 1000);//subtract amount of time since midnight
-    date += new Date().getTimezoneOffset() * 60 * 1000;//add on the timezone offset
-    return new Date(date);
-  }
+    const handleEndDate = (date) => {
+        filter(startDate, date);
+        dispatch({ type: "updateEndDate", endDate: date });
+    };
 
-  const filter = (start, end) => {
-    let swots = associate.swot;
-    swots = swots.filter(
-      (swot) =>
-        roundDate(new Date(swot.date_created)) >= roundDate(new Date(start)) &&
-        roundDate(new Date(swot.date_created)) <= roundDate(new Date(end))
-    );
-    dispatch({ type: "updateDisplaySwots", swots: swots });
-  };
-
-  const handleStartDate = (date) => {
-    filter(date, endDate);
-    dispatch({ type: "updateStartDate", startDate: date });
-  };
-
-  const handleEndDate = (date) => {
-    filter(startDate, date);
-    dispatch({ type: "updateEndDate", endDate: date });
-  };
-
-  const handleBack = (e) => {
-    e.preventDefault();
-    dispatch({ type: "updateAssociate", associate: {} });
-    dispatch({ type: "setStartDate", date: new Date(new Date().setDate(new Date().getDate() - 14))})
-    dispatch({ type: "setEndDate", date: new Date()})
-    history.push("/promotedlastweek");
-  };
-
-  const addSwot = () => {
-    const data = {
-      date_created: new Date(),
-      Strengths: [],
-      Weaknesses: [],
-      Opportunities: [],
-      Threats: [],
-      Notes: "",
+    const handleBack = (e) => {
+        e.preventDefault();
+        dispatch({ type: "updateAssociate", associate: {} });
+        dispatch({ type: "setStartDate", date: new Date(new Date().setDate(new Date().getDate() - 14))})
+        dispatch({ type: "setEndDate", date: new Date()})
+        history.push("/promotedlastweek");
     };
 
     const addSwot = () => {
         const data = {
-          date_created: new Date(),
-          Strengths: [],
-          Weaknesses: [],
-          Opportunities: [],
-          Threats: [],
-          Notes: ''
-        }
-        dispatch({type: 'updateEditable', editable: true})
-        dispatch({type: 'updateSWOT', SWOT: data})
-        history.push('/editSWOT')
-    }
+            date_created: new Date(),
+            Strengths: [],
+            Weaknesses: [],
+            Opportunities: [],
+            Threats: [],
+            Notes: "",
+        };
+        dispatch({ type: "updateEditable", editable: true });
+        dispatch({ type: "updateSWOT", SWOT: data });
+        history.push("/editSWOT");
+    };
 
     return (
-        <Menu size={'huge'} stackable>
-            <Menu.Item className="orange" header>
-                {associate.name}
-            </Menu.Item>
-            <Menu.Item className="orange" header>
-                {associate.batch_id}
-            </Menu.Item>
-            <Menu.Item className="orange">
-                <Button className="swot" onClick={() => addSwot()}>+ SWOT</Button>
-            </Menu.Item>
-            <Menu.Item className="orange" header>
-                Filter SWOTs by Date:
-            </Menu.Item>
-            <Menu.Item className="orange" header>
-                Start: &nbsp;<DatePicker selected={startDate} onChange={handleStartDate}></DatePicker>
-            </Menu.Item>
-            <Menu.Item className="orange" header>
-                End: &nbsp;<DatePicker selected={endDate} onChange={handleEndDate}></DatePicker>
-            </Menu.Item>
-        </Menu>
-    )
-}
+    <Menu size={"huge"} secondary>
+        <Menu.Item>
+            <Button icon onClick={handleBack}>
+                <Icon name={"arrow left"} />
+            </Button>
+        </Menu.Item>
+        <Menu.Item>{associate.name}</Menu.Item>
+        <Menu.Item>{associate.batch_id}</Menu.Item>
+        <Menu.Item>
+            <Button onClick={() => addSwot()}>+ SWOT</Button>
+        </Menu.Item>
+        <Menu.Item>
+            Start Date: &nbsp;
+            <DatePicker selected={startDate} onChange={handleStartDate}>
+            </DatePicker>
+            &nbsp; End Date: &nbsp;
+            <DatePicker selected={endDate} onChange={handleEndDate}>
+            </DatePicker>
+        </Menu.Item>
+    </Menu>
+    );
+};
 
 export default SwotsBar;

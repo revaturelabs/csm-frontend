9e6b871864ff73e1f3e7d89098dc2198a534ee75
jcarevature <Justin.Adams@uvm.edu>
2020-07-27 11:41:15 -0400

    Resolving merge conflicts in manager.service.js.

--- src/services/manager.service.js
+++ src/services/manager.service.js
@@ -1,15 +1,9 @@
 const { default: axios } = require('axios')
 
 class ManagerService {
-<<<<<<< HEAD
-    constructor() {
-        this.URI = 'http://localhost:5000/managers';
-    }
-=======
   constructor() {
     this.URI = "http://localhost:5000/managers";
   }
->>>>>>> 7ba30ffc46581d1e6d208365e45aca8e74bc14f4
 
   login(id) {
     // login to validate manager id

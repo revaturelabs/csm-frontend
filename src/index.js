6b207f03cc41e368355555a23f05f5abb0b9eda0
jcarevature <Justin.Adams@uvm.edu>
2020-07-27 11:41:57 -0400

    Resolving merge conflicts in index.js

--- src/index.js
+++ src/index.js
@@ -1,31 +1,3 @@
-<<<<<<< HEAD
-import React from "react";
-import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
-import { createStore } from "redux";
-import { composeWithDevTools } from "redux-devtools-extension";
-import "./index.css";
-import "react-datepicker/dist/react-datepicker.css";
-import App from "./App";
-import * as serviceWorker from "./serviceWorker";
-import rootReducer from "./reducers/index";
-
-const store = createStore(rootReducer, composeWithDevTools());
-
-ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById("root")
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
-=======
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
@@ -51,4 +23,3 @@ ReactDOM.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
->>>>>>> 7ba30ffc46581d1e6d208365e45aca8e74bc14f4
